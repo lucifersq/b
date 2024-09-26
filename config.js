cda13a65103e3e28523dee7b9359a740f2f95910
lucifersq <febridiin@gmail.com>
2024-09-26 11:42:15 +0700

    cccc

---
+++ config.js
@@ -0,0 +1,41 @@
+global.owner = ['6285179772608']  
+global.mods = ['6285179772608'] 
+global.prems = ['6285179772608']
+global.nameowner = 'Ray'
+global.numberowner = '6285179772608' 
+global.mail = 'support@tioprm.eu.org' 
+global.gc = 'https://chat.whatsapp.com/CP5mV2D9IO730HpBgmkuyi'
+global.instagram = 'https://www.instagram.com/rayrucc/'
+global.wm = '© Ray'
+global.wait = '_*Tunggu sedang di proses...*_'
+global.eror = '_*Server Error*_'
+global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
+global.packname = 'Made With'
+global.author = 'Bot WhatsApp'
+global.autobio = false // Set true untuk mengaktifkan autobio
+global.maxwarn = '2' // Peringatan maksimum
+
+//INI WAJIB DI ISI!//
+global.btc = 'Gorengan' 
+//Daftar terlebih dahulu https://api.botcahx.eu.org
+
+//INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
+global.lann = 'vPo4maEl'
+//Daftar https://api.betabotz.eu.org 
+
+global.APIs = {   
+  btc: 'https://api.botcahx.eu.org'
+}
+global.APIKeys = { 
+  'https://api.botcahx.eu.org': 'APIKEY' 
+}
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
